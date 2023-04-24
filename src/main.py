f9960d5ea0af786037fe0f5f8b1e0ed79c13b055
Fahad Hasmi <fahad.a.hasmi@gmail.com>
2023-04-24 22:45:24 +0530

    Changed the server files

--- src/main.py
+++ src/main.py
@@ -1,13 +1,20 @@
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
 from data_transform import transform
 from model_prediction import predict
 
 app = FastAPI()
 
+origins = ["*"]
 
-@app.get('/')
-def index():
-    return "Index Page"
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.post('/')
 async def index(file: UploadFile = File(...)):
